ae44b30698450ea2129e4b4c9d09b5a9e5f471b8
Ubuntu <ubuntu@ip-172-31-43-133.ec2.internal>
2020-05-14 05:14:18 +0000

    change setup

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ pip3 install -r requirements.txt --no-cache-dir
 #mkdir ~/.aws
 #cp credentials ~/.aws/credentials
 #python3 boto.py
-export PYTHONPATH=/home/$USER/image-quality-assessment/src
+#export PYTHONPATH=/home/$USER/image-quality-assessment/src
 python3 main.py
